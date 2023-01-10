5f5a9209cc4971cc11a133e753687ff98c7ea5fe
Francis CLAIRICIA-ROSE-CLAIRE-JOSEPHINE <francis.clairicia-rose-claire-josephine@epitech.eu>
2023-01-10 09:54:31 +0100

    [FIX] PickleSerializer: Added previously removed get_picker() and get_unpickler() methods

--- src/easynetwork/serializers/pickle.py
+++ src/easynetwork/serializers/pickle.py
@@ -14,7 +14,7 @@ from dataclasses import asdict as dataclass_asdict, dataclass
 from io import BytesIO
 from pickle import DEFAULT_PROTOCOL, STOP as STOP_OPCODE, Pickler, Unpickler, UnpicklingError
 from pickletools import optimize as pickletools_optimize
-from typing import IO, Any, TypeVar, final
+from typing import IO, Any, Literal, TypeVar, final
 
 from .stream.abc import FileBasedIncrementalPacketSerializer
 
@@ -57,32 +57,48 @@ class PickleSerializer(FileBasedIncrementalPacketSerializer[_ST_contra, _DT_co])
         if pickler is None:
             pickler = PicklerConfig()
         elif not isinstance(pickler, PicklerConfig):
-            raise TypeError(f"Invalid encoder: expected {PicklerConfig.__name__}, got {type(pickler).__name__}")
+            raise TypeError(f"Invalid pickler config: expected {PicklerConfig.__name__}, got {type(pickler).__name__}")
         self.__pickler_conf = dataclass_asdict(pickler)
 
         if unpickler is None:
             unpickler = UnpicklerConfig()
         elif not isinstance(unpickler, UnpicklerConfig):
-            raise TypeError(f"Invalid decoder: expected {UnpicklerConfig.__name__}, got {type(unpickler).__name__}")
+            raise TypeError(f"Invalid unpickler config: expected {UnpicklerConfig.__name__}, got {type(unpickler).__name__}")
         self.__unpickler_conf = dataclass_asdict(unpickler)
 
     @final
     def _serialize_to_file(self, packet: _ST_contra, file: IO[bytes]) -> None:
         if not self.__optimize:
-            pickler = Pickler(file, **self.__pickler_conf, buffer_callback=None)
+            pickler = self.get_pickler(file, **self.__pickler_conf)
+            assert isinstance(pickler, Pickler)
             pickler.dump(packet)
             return
         with BytesIO() as buffer:
-            pickler = Pickler(buffer, **self.__pickler_conf, buffer_callback=None)
+            pickler = self.get_pickler(buffer, **self.__pickler_conf)
+            assert isinstance(pickler, Pickler)
             pickler.dump(packet)
             file.write(pickletools_optimize(buffer.getvalue()))
 
     @final
     def _deserialize_from_file(self, file: IO[bytes]) -> _DT_co:
-        unpickler = Unpickler(file, **self.__unpickler_conf, errors="strict", buffers=None)
+        unpickler = self.get_unpickler(file, **self.__unpickler_conf, errors="strict")
+        assert isinstance(unpickler, Unpickler)
         packet: _DT_co = unpickler.load()
         return packet
 
     @final
     def wait_for_next_chunk(self, given_chunk: bytes) -> bool:
         return STOP_OPCODE not in given_chunk
+
+    def get_pickler(self, file: IO[bytes], *, protocol: int, fix_imports: bool) -> Pickler:
+        return Pickler(file, protocol=protocol, fix_imports=fix_imports, buffer_callback=None)
+
+    def get_unpickler(
+        self,
+        file: IO[bytes],
+        *,
+        fix_imports: bool,
+        encoding: str,
+        errors: Literal["strict", "error", "replace"],
+    ) -> Unpickler:
+        return Unpickler(file, fix_imports=fix_imports, encoding=encoding, errors=errors, buffers=None)
