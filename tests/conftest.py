300da4eb3e719d4c8e1064beca3c0c7038312b1b
Francis CLAIRICIA-ROSE-CLAIRE-JOSEPHINE <francis.clairicia-rose-claire-josephine@epitech.eu>
2023-04-11 20:34:15 +0200

    [TESTS] Modified testenv in tox.ini

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,7 @@
 
 from __future__ import annotations
 
+import os
 import random
 
 random.seed(42)  # Fully deterministic random output
@@ -10,6 +11,13 @@ import functools
 import sys
 
 import pytest
+
+
+def pytest_report_header() -> list[str]:
+    addopts: str = os.environ.get("PYTEST_ADDOPTS", "")
+    if not addopts:
+        return []
+    return [f"PYTEST_ADDOPTS: {addopts}"]
 
 
 @functools.cache
