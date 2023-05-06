83570a88a38364fa94d24b8167a68a27722850a3
Francis CLAIRICIA-ROSE-CLAIRE-JOSEPHINE <francis.clairicia-rose-claire-josephine@epitech.eu>
2023-05-06 16:49:29 +0200

    [TESTS] tox: Added asyncio-proactor testenv

--- tests/pytest_plugins/asyncio_event_loop.py
+++ tests/pytest_plugins/asyncio_event_loop.py
@@ -11,6 +11,7 @@ import pytest
 
 class EventLoop(enum.StrEnum):
     ASYNCIO = "asyncio"
+    ASYNCIO_PROACTOR = "asyncio-proactor"
     UVLOOP = "uvloop"
 
 
@@ -31,6 +32,10 @@ def _get_windows_selector_policy() -> type[asyncio.AbstractEventLoopPolicy] | No
     return getattr(asyncio, "WindowsSelectorEventLoopPolicy", None)
 
 
+def _get_windows_proactor_policy() -> type[asyncio.AbstractEventLoopPolicy] | None:
+    return getattr(asyncio, "WindowsProactorEventLoopPolicy", None)
+
+
 def _set_event_loop_policy_according_to_configuration(config: pytest.Config) -> None:
     event_loop: EventLoop = config.getoption(ASYNCIO_EVENT_LOOP_OPTION)
     match event_loop:
@@ -38,6 +43,11 @@ def _set_event_loop_policy_according_to_configuration(config: pytest.Config) -> 
             WindowsSelectorEventLoopPolicy = _get_windows_selector_policy()
             if WindowsSelectorEventLoopPolicy is not None:
                 asyncio.set_event_loop_policy(WindowsSelectorEventLoopPolicy())
+        case EventLoop.ASYNCIO_PROACTOR:
+            WindowsProactorEventLoopPolicy = _get_windows_proactor_policy()
+            if WindowsProactorEventLoopPolicy is None:
+                raise pytest.UsageError(f"{event_loop} event loop is not available in this platform")
+            asyncio.set_event_loop_policy(WindowsProactorEventLoopPolicy())
         case EventLoop.UVLOOP:
             uvloop: Any = pytest.importorskip("uvloop")
 
