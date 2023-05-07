fc34cc5c726abfd16542e988a6da9046da5d90f4
Francis Clairicia-Rose-Claire-Joséphine <francis.clairicia-rose-claire-josephine@epitech.eu>
2023-05-07 15:01:40 +0200

    Updated project metadata (#31)

---
+++ tests/other_test/test_project_metadata.py
@@ -0,0 +1,19 @@
+# -*- coding: Utf-8 -*-
+
+from __future__ import annotations
+
+import importlib.metadata as importlib_metadata
+
+import pytest
+import trove_classifiers
+
+
+@pytest.fixture(scope="module")
+def easynetwork_metadata() -> importlib_metadata.PackageMetadata:
+    return importlib_metadata.metadata("easynetwork")
+
+
+def test____project____classifiers_all_are_valids(easynetwork_metadata: importlib_metadata.PackageMetadata) -> None:
+    unknown_classifiers = set(easynetwork_metadata.get_all("Classifier", [])).difference(trove_classifiers.classifiers)
+
+    assert not unknown_classifiers
