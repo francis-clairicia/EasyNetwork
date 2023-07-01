472e322687516fd22909ffc618a0223f9fa835b9
Francis Clairicia-Rose-Claire-Joséphine <francis.clairicia-rose-claire-josephine@epitech.eu>
2023-07-01 17:42:03 +0200

    Removed lock in StreamDataConsumer and StreamDataProducer (#78)

--- src/easynetwork/tools/stream.py
+++ src/easynetwork/tools/stream.py
@@ -11,13 +11,11 @@ __all__ = [
     "StreamDataProducer",
 ]
 
-import threading
 from collections import deque
 from typing import Any, Generator, Generic, Iterator, TypeVar, final
 
 from ..exceptions import StreamProtocolParseError
 from ..protocol import StreamProtocol
-from .lock import ForkSafeLock
 
 _SentPacketT = TypeVar("_SentPacketT")
 _ReceivedPacketT = TypeVar("_ReceivedPacketT")
@@ -26,7 +24,7 @@ _ReceivedPacketT = TypeVar("_ReceivedPacketT")
 @final
 @Iterator.register
 class StreamDataProducer(Generic[_SentPacketT]):
-    __slots__ = ("__p", "__g", "__q", "__lock")
+    __slots__ = ("__p", "__g", "__q")
 
     def __init_subclass__(cls) -> None:  # pragma: no cover
         raise TypeError("StreamDataProducer cannot be subclassed")
@@ -37,8 +35,6 @@ class StreamDataProducer(Generic[_SentPacketT]):
         self.__p: StreamProtocol[_SentPacketT, Any] = protocol
         self.__g: Generator[bytes, None, None] | None = None
         self.__q: deque[_SentPacketT] = deque()
-
-        self.__lock = ForkSafeLock(threading.Lock)
 
     def __del__(self) -> None:  # pragma: no cover
         try:
@@ -55,49 +51,43 @@ class StreamDataProducer(Generic[_SentPacketT]):
         return self
 
     def __next__(self) -> bytes:
-        with self.__lock.get():
-            protocol = self.__p
-            queue: deque[_SentPacketT] = self.__q
-            generator: Generator[bytes, None, None] | None
-            while (generator := self.__g) is not None or queue:
-                if generator is None:
-                    generator = protocol.generate_chunks(queue.popleft())
-                else:
-                    self.__g = None
-                try:
-                    chunk = next(filter(None, generator))
-                except StopIteration:
-                    pass
-                else:
-                    assert isinstance(chunk, (bytes, bytearray))
-                    self.__g = generator
-                    return chunk
-                finally:
-                    del generator
-            raise StopIteration
+        protocol = self.__p
+        queue: deque[_SentPacketT] = self.__q
+        generator: Generator[bytes, None, None] | None
+        while (generator := self.__g) is not None or queue:
+            if generator is None:
+                generator = protocol.generate_chunks(queue.popleft())
+            else:
+                self.__g = None
+            try:
+                chunk = next(filter(None, generator))
+            except StopIteration:
+                pass
+            else:
+                assert isinstance(chunk, (bytes, bytearray))
+                self.__g = generator
+                return chunk
+            finally:
+                del generator
+        raise StopIteration
 
     def pending_packets(self) -> bool:
-        with self.__lock.get():
-            return self.__g is not None or bool(self.__q)
+        return self.__g is not None or bool(self.__q)
 
     def queue(self, *packets: _SentPacketT) -> None:
-        if not packets:
-            return
-        with self.__lock.get():
-            self.__q.extend(packets)
+        self.__q.extend(packets)
 
     def clear(self) -> None:
-        with self.__lock.get():
-            self.__q.clear()
-            generator, self.__g = self.__g, None
-            if generator is not None:
-                generator.close()
+        self.__q.clear()
+        generator, self.__g = self.__g, None
+        if generator is not None:
+            generator.close()
 
 
 @final
 @Iterator.register
 class StreamDataConsumer(Generic[_ReceivedPacketT]):
-    __slots__ = ("__p", "__b", "__c", "__lock")
+    __slots__ = ("__p", "__b", "__c")
 
     def __init_subclass__(cls) -> None:  # pragma: no cover
         raise TypeError("StreamDataConsumer cannot be subclassed")
@@ -108,8 +98,6 @@ class StreamDataConsumer(Generic[_ReceivedPacketT]):
         self.__p: StreamProtocol[Any, _ReceivedPacketT] = protocol
         self.__c: Generator[None, bytes, tuple[_ReceivedPacketT, bytes]] | None = None
         self.__b: bytes = b""
-
-        self.__lock = ForkSafeLock(threading.Lock)
 
     def __del__(self) -> None:  # pragma: no cover
         try:
@@ -126,48 +114,45 @@ class StreamDataConsumer(Generic[_ReceivedPacketT]):
         return self
 
     def __next__(self) -> _ReceivedPacketT:
-        with self.__lock.get():
-            chunk: bytes = self.__b
-            if not chunk:
-                raise StopIteration
-            consumer, self.__c = self.__c, None
-            if consumer is None:
-                consumer = self.__p.build_packet_from_chunks()
-                try:
-                    next(consumer)
-                except StopIteration:
-                    raise RuntimeError("protocol.build_packet_from_chunks() did not yield") from None
-            self.__b = b""
-            packet: _ReceivedPacketT
+        chunk: bytes = self.__b
+        if not chunk:
+            raise StopIteration
+        consumer, self.__c = self.__c, None
+        if consumer is None:
+            consumer = self.__p.build_packet_from_chunks()
             try:
-                consumer.send(chunk)
-            except StopIteration as exc:
-                packet, chunk = exc.value
-            except StreamProtocolParseError as exc:
-                self.__b, exc.remaining_data = bytes(exc.remaining_data), b""
-                raise
-            else:
-                self.__c = consumer
-                raise StopIteration
-            finally:
-                del consumer
-            self.__b = bytes(chunk)
-            return packet
+                next(consumer)
+            except StopIteration:
+                raise RuntimeError("protocol.build_packet_from_chunks() did not yield") from None
+        self.__b = b""
+        packet: _ReceivedPacketT
+        remaining: bytes
+        try:
+            consumer.send(chunk)
+        except StopIteration as exc:
+            packet, remaining = exc.value
+        except StreamProtocolParseError as exc:
+            self.__b, exc.remaining_data = bytes(exc.remaining_data), b""
+            raise
+        else:
+            self.__c = consumer
+            raise StopIteration
+        finally:
+            del consumer, chunk
+        self.__b = bytes(remaining)
+        return packet
 
     def feed(self, chunk: bytes) -> None:
         assert isinstance(chunk, (bytes, bytearray))
         if not chunk:
             return
-        with self.__lock.get():
-            self.__b += chunk
+        self.__b += chunk
 
     def get_buffer(self) -> bytes:
-        with self.__lock.get():
-            return self.__b
+        return self.__b
 
     def clear(self) -> None:
-        with self.__lock.get():
-            self.__b = b""
-            consumer, self.__c = self.__c, None
-            if consumer is not None:
-                consumer.close()
+        self.__b = b""
+        consumer, self.__c = self.__c, None
+        if consumer is not None:
+            consumer.close()
