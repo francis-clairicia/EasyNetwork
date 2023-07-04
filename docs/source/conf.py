5ceb89e13ed880cd15fdb77dc7c0bb87f3921460
Francis CLAIRICIA-ROSE-CLAIRE-JOSEPHINE <francis.clairicia-rose-claire-josephine@epitech.eu>
2023-07-04 08:21:35 +0200

    [DOCS] sphinx-quickstart done

---
+++ docs/source/conf.py
@@ -0,0 +1,30 @@
+# type: ignore
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+project = "EasyNetwork"
+copyright = "2023, FrankySnow9"
+author = "FrankySnow9"
+release = "1.0.0rc4"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "sphinx_rtd_theme",
+    "sphinx_tabs.tabs",
+]
+
+templates_path = []
+exclude_patterns = []
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "sphinx_rtd_theme"
+html_static_path = []
---
+++ docs/source/index.rst
@@ -0,0 +1,20 @@
+.. EasyNetwork documentation master file, created by
+   sphinx-quickstart on Tue Jul  4 06:42:37 2023.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to EasyNetwork's documentation!
+=======================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
