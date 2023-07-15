b48d3c5db8b45a5a57ade5525e9a3c90042fa005
Francis CLAIRICIA-ROSE-CLAIRE-JOSEPHINE <francis.clairicia-rose-claire-josephine@epitech.eu>
2023-07-15 13:17:49 +0200

    [PROJECT] Added license header to all source files

--- src/easynetwork/__init__.py
+++ src/easynetwork/__init__.py
@@ -1,4 +1,15 @@
-# Copyright (c) 2021-2023, Francis Clairicia-Rose-Claire-Josephine
+# Copyright 2021-2023, Francis Clairicia-Rose-Claire-Josephine
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 #
 #
 """The easiest way to use sockets in Python
@@ -12,11 +23,11 @@ __all__ = []  # type: list[str]
 
 __author__ = "FrankySnow9"
 __contact__ = "clairicia.rcj.francis@gmail.com"
-__copyright__ = "Copyright (c) 2021-2023, Francis Clairicia-Rose-Claire-Josephine"
+__copyright__ = "Copyright 2021-2023, Francis Clairicia-Rose-Claire-Josephine"
 __credits__ = ["FrankySnow9"]
 __deprecated__ = False
 __email__ = "clairicia.rcj.francis@gmail.com"
-__license__ = "MIT"
+__license__ = "Apache 2.0"
 __maintainer__ = "FrankySnow9"
 __status__ = "Development"
 __version__ = "1.0.0rc4"
