francis-clairicia/EasyNetwork

commit be99a705e81375c48067e69d52bdc77638e1ee14

Added `backend.spawn_task()` method (#100)

src/easynetwork/api_async/backend/abc.py
@@ -321,6 +321,16 @@ class AbstractAsyncBackend(metaclass=ABCMeta):
         return await self.sleep(max(deadline - self.current_time(), 0))
 
     @abstractmethod
+    def spawn_task(
+        self,
+        coro_func: Callable[_P, Coroutine[Any, Any, _T]],
+        /,
+        *args: _P.args,
+        **kwargs: _P.kwargs,
+    ) -> AbstractTask[_T]:
+        raise NotImplementedError
+
+    @abstractmethod
     def create_task_group(self) -> AbstractTaskGroup:
         raise NotImplementedError
 

src/easynetwork/api_async/backend/tasks.py
@@ -53,23 +53,17 @@ class SingleTaskRunner(Generic[_T_co]):
         return True
 
     async def run(self) -> _T_co:
-        if self.__task is not None:
-            return await self.__task.join()
+        if self.__task is None:
+            if self.__coro_func is None:
+                self.__task = self.__backend.spawn_task(self.__backend.sleep_forever)
+                self.__task.cancel()
+            else:
+                coro_func, args, kwargs = self.__coro_func
+                self.__coro_func = None
+                self.__task = self.__backend.spawn_task(coro_func, *args, **kwargs)
+                del coro_func, args, kwargs
 
         try:
-            async with self.__backend.create_task_group() as task_group:
-                if self.__coro_func is None:
-                    self.__task = task_group.start_soon(self.__backend.sleep_forever)
-                    self.__task.cancel()
-                else:
-                    coro_func, args, kwargs = self.__coro_func
-                    self.__coro_func = None
-                    self.__task = task_group.start_soon(coro_func, *args, **kwargs)
-                    del coro_func, args, kwargs
-        except BaseExceptionGroup as excgrp:
-            if len(excgrp.exceptions) != 1:  # pragma: no cover  # Hard to test
-                raise
-            # This is most likely the unhandled exception raised by coro_func
-            raise excgrp.exceptions[0] from None
-
-        return await self.__task.join()
+            return await self.__task.join()
+        finally:
+            del self  # Avoid circular reference with raised exception (if any)

src/easynetwork_asyncio/backend.py
@@ -38,7 +38,7 @@ from .datagram.endpoint import create_datagram_endpoint
 from .datagram.socket import AsyncioTransportDatagramSocketAdapter, RawDatagramSocketAdapter
 from .stream.listener import AcceptedSocket, AcceptedSSLSocket, ListenerSocketAdapter
 from .stream.socket import AsyncioTransportStreamSocketAdapter, RawStreamSocketAdapter
-from .tasks import TaskGroup, timeout, timeout_at
+from .tasks import Task, TaskGroup, timeout, timeout_at
 from .threads import ThreadsPortal
 
 if TYPE_CHECKING:
@@ -77,13 +77,18 @@ class AsyncioBackend(AbstractAsyncBackend):
 
     async def cancel_shielded_coro_yield(self) -> None:
         current_task: asyncio.Task[Any] = self._current_asyncio_task()
-        cancelling: int = current_task.cancelling()
         try:
             await asyncio.sleep(0)
-        except asyncio.CancelledError:
-            assert current_task.cancelling() > cancelling
-            while current_task.uncancel() > cancelling:
-                continue
+        except asyncio.CancelledError as exc:
+            msg: str | None
+            if exc.args:
+                msg = exc.args[0]
+            else:
+                msg = None
+            # uncancel so the Task object is aware we have explicitly caught the exception...
+            current_task.uncancel()
+            # ...but cancel it again so the next step will throw a CancelledError
+            current_task.get_loop().call_soon(current_task.cancel, msg)
         finally:
             del current_task
 
@@ -136,6 +141,15 @@ class AsyncioBackend(AbstractAsyncBackend):
         loop = asyncio.get_running_loop()
         await loop.create_future()
         raise AssertionError("await an unused future cannot end in any other way than by cancellation")
+
+    def spawn_task(
+        self,
+        coro_func: Callable[_P, Coroutine[Any, Any, _T]],
+        /,
+        *args: _P.args,
+        **kwargs: _P.kwargs,
+    ) -> Task[_T]:
+        return Task(asyncio.create_task(coro_func(*args, **kwargs)))
 
     def create_task_group(self) -> TaskGroup:
         return TaskGroup()

tests/functional_test/test_async/test_backend/test_asyncio_backend.py
@@ -38,6 +38,40 @@ class TestAsyncioBackend:
 
         await task
         assert not task.cancelled()
+        assert task.cancelling() == 2
+
+    @pytest.mark.parametrize("cancel_message", ["something", None], ids=lambda p: f"cancel_message=={p!r}")
+    async def test____cancel_shielded_coro_yield____cancel_at_the_next_checkpoint(
+        self,
+        cancel_message: str | None,
+        event_loop: asyncio.AbstractEventLoop,
+        backend: AsyncioBackend,
+    ) -> None:
+        test_list: list[str] = []
+
+        async def coroutine() -> None:
+            test_list.append("a")
+            await backend.cancel_shielded_coro_yield()
+            test_list.append("b")
+            await backend.coro_yield()
+            test_list.append("this should not be in the list")
+
+        task: asyncio.Task[None] = event_loop.create_task(coroutine())
+
+        await asyncio.sleep(0)
+
+        for _ in range(3):
+            task.cancel(msg=cancel_message)
+
+        with pytest.raises(asyncio.CancelledError) as exc_info:
+            await task
+        assert task.cancelled()
+        assert task.cancelling() == 3
+        assert test_list == ["a", "b"]
+        if cancel_message is None:
+            assert exc_info.value.args == ()
+        else:
+            assert exc_info.value.args == (cancel_message,)
 
     async def test____ignore_cancellation____always_continue_on_cancellation(
         self,
@@ -158,6 +192,48 @@ class TestAsyncioBackend:
 
         with pytest.raises(asyncio.CancelledError):
             await sleep_task
+
+    async def test____spawn_task____run_coroutine_in_background(
+        self,
+        backend: AsyncioBackend,
+    ) -> None:
+        async def coroutine(value: int) -> int:
+            return await asyncio.sleep(0.5, value)
+
+        task = backend.spawn_task(coroutine, 42)
+        await asyncio.sleep(1)
+        assert task.done()
+        assert await task.join() == 42
+
+    async def test____spawn_task____task_cancellation(
+        self,
+        event_loop: asyncio.AbstractEventLoop,
+        backend: AsyncioBackend,
+    ) -> None:
+        async def coroutine(value: int) -> int:
+            return await asyncio.sleep(0.5, value)
+
+        task = backend.spawn_task(coroutine, 42)
+
+        event_loop.call_later(0.2, task.cancel)
+
+        with pytest.raises(asyncio.CancelledError):
+            await task.join()
+
+        assert task.cancelled()
+
+    async def test____spawn_task____exception(
+        self,
+        backend: AsyncioBackend,
+    ) -> None:
+        async def coroutine(value: int) -> int:
+            await asyncio.sleep(0.1)
+            raise ZeroDivisionError
+
+        task = backend.spawn_task(coroutine, 42)
+
+        with pytest.raises(ZeroDivisionError):
+            await task.join()
 
     async def test____create_task_group____task_pool(
         self,

tests/unit_test/test_async/conftest.py
@@ -36,13 +36,14 @@ def fake_cancellation_cls() -> type[BaseException]:
 
 @pytest.fixture
 def mock_backend(fake_cancellation_cls: type[BaseException], mocker: MockerFixture) -> MagicMock:
-    from easynetwork_asyncio.tasks import TaskGroup
+    from easynetwork_asyncio.tasks import Task, TaskGroup
 
     from .._utils import AsyncDummyLock
 
     mock_backend = mocker.NonCallableMagicMock(spec=AbstractAsyncBackend)
 
     mock_backend.get_cancelled_exc_class.return_value = fake_cancellation_cls
+    mock_backend.spawn_task = lambda coro_func, *args, **kwargs: Task(asyncio.create_task(coro_func(*args, **kwargs)))
     mock_backend.create_lock = AsyncDummyLock
     mock_backend.create_event = asyncio.Event
     mock_backend.create_task_group = TaskGroup

tests/unit_test/test_async/test_api/test_backend/_fake_backends.py
@@ -9,6 +9,7 @@ from easynetwork.api_async.backend.abc import (
     AbstractAsyncDatagramSocketAdapter,
     AbstractAsyncListenerSocketAdapter,
     AbstractAsyncStreamSocketAdapter,
+    AbstractTask,
     AbstractTaskGroup,
     AbstractThreadsPortal,
     AbstractTimeoutHandle,
@@ -47,6 +48,9 @@ class BaseFakeBackend(AbstractAsyncBackend):
         raise NotImplementedError
 
     def get_cancelled_exc_class(self) -> type[BaseException]:
+        raise NotImplementedError
+
+    def spawn_task(self, *args: Any, **kwargs: Any) -> AbstractTask[Any]:
         raise NotImplementedError
 
     def create_task_group(self) -> AbstractTaskGroup:

commit 4e01d839e8aaf52132f848c7c8e8d120734b4dc0

Fixed regressions after dev made in PR #100 (#101)

src/easynetwork/api_async/backend/abc.py
@@ -117,6 +117,10 @@ class AbstractTask(Generic[_T_co], metaclass=ABCMeta):
     async def join(self) -> _T_co:
         raise NotImplementedError
 
+    @abstractmethod
+    async def join_or_cancel(self) -> _T_co:
+        raise NotImplementedError
+
 
 class AbstractTaskGroup(metaclass=ABCMeta):
     __slots__ = ("__weakref__",)

src/easynetwork/api_async/backend/tasks.py
@@ -53,6 +53,7 @@ class SingleTaskRunner(Generic[_T_co]):
         return True
 
     async def run(self) -> _T_co:
+        must_cancel_inner_task: bool = False
         if self.__task is None:
             if self.__coro_func is None:
                 self.__task = self.__backend.spawn_task(self.__backend.sleep_forever)
@@ -62,8 +63,12 @@ class SingleTaskRunner(Generic[_T_co]):
                 self.__coro_func = None
                 self.__task = self.__backend.spawn_task(coro_func, *args, **kwargs)
                 del coro_func, args, kwargs
+                must_cancel_inner_task = True
 
         try:
-            return await self.__task.join()
+            if must_cancel_inner_task:
+                return await self.__task.join_or_cancel()
+            else:
+                return await self.__task.join()
         finally:
             del self  # Avoid circular reference with raised exception (if any)

src/easynetwork_asyncio/tasks.py
@@ -53,19 +53,31 @@ class Task(AbstractTask[_T_co]):
         return self.__t.cancelled()
 
     async def wait(self) -> None:
-        if self.__t.done():
-            return
-        await asyncio.wait({self.__t})
+        task = self.__t
+        try:
+            if task.done():
+                return
+            await asyncio.wait({task})
+        finally:
+            del task, self  # This is needed to avoid circular reference with raised exception
 
     async def join(self) -> _T_co:
         # If the caller cancels the join() task, it should not stop the inner task
         # e.g. when awaiting from an another task than the one which creates the TaskGroup,
         #      you want to stop joining the sub-task, not accidentally cancel it.
         # It is primarily to avoid error prone code where tasks were not explicitly cancelled using task.cancel()
+        task = self.__t
         try:
-            return await asyncio.shield(self.__t)
+            return await asyncio.shield(task)
         finally:
-            del self  # This is needed to avoid circular reference with raised exception
+            del task, self  # This is needed to avoid circular reference with raised exception
+
+    async def join_or_cancel(self) -> _T_co:
+        task = self.__t
+        try:
+            return await task
+        finally:
+            del task, self  # This is needed to avoid circular reference with raised exception
 
 
 @final

tests/functional_test/test_async/test_backend/test_tasks.py
@@ -86,3 +86,38 @@ class TestSingleTaskRunner:
 
         assert exc_info_run_1.value is my_exc
         assert exc_info_run_2.value is my_exc
+
+    async def test____run____waiting_task_is_cancelled(self, backend: AbstractAsyncBackend) -> None:
+        inner_task: list[asyncio.Task[int] | None] = []
+
+        async def coro_func(value: int) -> int:
+            inner_task.append(asyncio.current_task())
+            return await asyncio.sleep(3600, value)
+
+        runner: SingleTaskRunner[Any] = SingleTaskRunner(backend, coro_func, 42)
+
+        task = asyncio.create_task(runner.run())
+        await asyncio.sleep(0.1)
+
+        task.cancel()
+
+        with pytest.raises(asyncio.CancelledError):
+            await task
+
+        assert inner_task[0] is not None and inner_task[0].cancelled()
+
+    async def test____run____waiting_task_is_cancelled____not_the_first_runner(self, backend: AbstractAsyncBackend) -> None:
+        async def coro_func(value: int) -> int:
+            return await asyncio.sleep(0.5, value)
+
+        runner: SingleTaskRunner[Any] = SingleTaskRunner(backend, coro_func, 42)
+
+        task = asyncio.create_task(runner.run())
+        task_2 = asyncio.create_task(runner.run())
+        await asyncio.sleep(0.1)
+
+        task_2.cancel()
+
+        assert await task == 42
+        with pytest.raises(asyncio.CancelledError):
+            await task_2

tests/functional_test/test_communication/test_async/test_server/base.py
@@ -37,6 +37,18 @@ class BaseTestAsyncServer:
         with pytest.raises(ServerClosedError):
             await server.serve_forever()
 
+    async def test____serve_forever____shutdown_while_setup(
+        self,
+        server: AbstractAsyncNetworkServer,
+    ) -> None:
+        event = asyncio.Event()
+        async with asyncio.TaskGroup() as tg:
+            _ = tg.create_task(server.serve_forever(is_up_event=event))
+            await asyncio.sleep(0)
+            assert not event.is_set()
+            await server.shutdown()
+            assert event.is_set()
+
     async def test____serve_forever____concurrent_shutdown(
         self,
         server: AbstractAsyncNetworkServer,