45ed038d8fb93f494a4596034f35e89db87e90d4
Francis CLAIRICIA-ROSE-CLAIRE-JOSEPHINE <francis.clairicia-rose-claire-josephine@epitech.eu>
2023-07-24 06:18:07 +0200

    [UPDATE] Removed IEvent.clear()

--- src/easynetwork/api_async/server/tcp.py
+++ src/easynetwork/api_async/server/tcp.py
@@ -201,8 +201,8 @@ class AsyncTCPNetworkServer(AbstractAsyncNetworkServer, Generic[_RequestT, _Resp
 
         async with _contextlib.AsyncExitStack() as server_exit_stack:
             # Wake up server
-            self.__is_shutdown.clear()
-            server_exit_stack.callback(self.__is_shutdown.set)
+            self.__is_shutdown = is_shutdown = self.__backend.create_event()
+            server_exit_stack.callback(is_shutdown.set)
             if is_up_event is not None:
                 # Force is_up_event to be set, in order not to stuck the waiting task
                 server_exit_stack.callback(is_up_event.set)
@@ -239,8 +239,9 @@ class AsyncTCPNetworkServer(AbstractAsyncNetworkServer, Generic[_RequestT, _Resp
             ##################
 
             # Enable listener
-            for listener in self.__listeners:
-                self.__listener_tasks.append(task_group.start_soon(self.__listener_task, listener, task_group))
+            self.__listener_tasks.extend(
+                task_group.start_soon(self.__listener_task, listener, task_group) for listener in self.__listeners
+            )
             self.__logger.info("Start serving at %s", ", ".join(map(str, self.get_addresses())))
             #################
 
