c09ae10649e61580c003ac1fa4b2aedecd9c1cc5
Francis CLAIRICIA-ROSE-CLAIRE-JOSEPHINE <francis.clairicia-rose-claire-josephine@epitech.eu>
2023-09-02 15:52:04 +0200

    [DOCS] Added AsyncExecutor documentation

--- src/easynetwork/api_async/backend/futures.py
+++ src/easynetwork/api_async/backend/futures.py
@@ -35,9 +35,41 @@ _T = TypeVar("_T")
 
 
 class AsyncExecutor:
+    """
+    Wraps a :class:`concurrent.futures.Executor` instance.
+
+
+    For example, this code::
+
+        from concurrent.futures import ProcessPoolExecutor, wait
+
+        def main() -> None:
+            with ProcessPoolExecutor() as executor:
+                futures = [executor.submit(pow, a, b) for a, b in [(3, 4), (12, 2), (6, 8)]]
+                wait(futures)
+                results = [f.result() for f in futures]
+
+    can be converted to::
+
+        from concurrent.futures import ProcessPoolExecutor
+
+        async def main() -> None:
+            ...
+
+            async with AsyncExecutor(backend, ProcessPoolExecutor()) as executor:
+                async with backend.create_task_group() as task_group:
+                    tasks = [task_group.start_soon(executor.run, pow, a, b) for a, b in [(3, 4), (12, 2), (6, 8)]]
+                results = [await t.join() for t in tasks]
+    """
+
     __slots__ = ("__backend", "__executor", "__weakref__")
 
     def __init__(self, backend: AsyncBackend, executor: concurrent.futures.Executor) -> None:
+        """
+        Parameters:
+            backend: The asynchronous backend interface.
+            executor: The executor instance to wrap.
+        """
         self.__backend: AsyncBackend = backend
         self.__executor: concurrent.futures.Executor = executor
 
@@ -50,22 +82,72 @@ class AsyncExecutor:
         exc_val: BaseException | None,
         exc_tb: TracebackType | None,
     ) -> None:
+        """Calls :meth:`shutdown`."""
         await self.shutdown()
 
     async def run(self, func: Callable[_P, _T], /, *args: _P.args, **kwargs: _P.kwargs) -> _T:
+        """
+        Executes ``func(*args, **kwargs)`` in the executor, blocking until it is complete.
+
+        Example::
+
+            async with AsyncExecutor(backend, ThreadPoolExecutor(max_workers=1)) as executor:
+                result = await executor.run(pow, 323, 1235)
+
+        Parameters:
+            func: A synchronous function.
+            args: Positional arguments to be passed to `func`.
+            kwargs: Keyword arguments to be passed to `func`.
+
+        Raises:
+            RuntimeError: if the executor is closed.
+            Exception: Whatever raises ``func(*args, **kwargs)``
+
+        Returns:
+            Whatever returns ``func(*args, **kwargs)``
+        """
         try:
             return await self.__backend.wait_future(self.__executor.submit(func, *args, **kwargs))
         finally:
             del func, args, kwargs
 
     def shutdown_nowait(self, *, cancel_futures: bool = False) -> None:
+        """
+        Signal the executor that it should free any resources that it is using when the currently pending futures
+        are done executing.
+
+        Calls to :meth:`AsyncExecutor.run` made after shutdown will raise :exc:`RuntimeError`.
+
+        Parameters:
+            cancel_futures: If :data:`True`, this method will cancel all pending futures that the executor
+                            has not started running. Any futures that are completed or running won't be cancelled,
+                            regardless of the value of `cancel_futures`.
+        """
         self.__executor.shutdown(wait=False, cancel_futures=cancel_futures)
 
     async def shutdown(self, *, cancel_futures: bool = False) -> None:
+        """
+        Signal the executor that it should free any resources that it is using when the currently pending futures
+        are done executing.
+
+        Calls to :meth:`AsyncExecutor.run` made after shutdown will raise :exc:`RuntimeError`.
+
+        This method will block until all the pending futures are done executing and
+        the resources associated with the executor have been freed.
+
+        Parameters:
+            cancel_futures: If :data:`True`, this method will cancel all pending futures that the executor
+                            has not started running. Any futures that are completed or running won't be cancelled,
+                            regardless of the value of `cancel_futures`.
+        """
         await self.__backend.run_in_thread(self.__executor.shutdown, wait=True, cancel_futures=cancel_futures)
 
 
 class AsyncThreadPoolExecutor(AsyncExecutor):
+    """
+    :class:`AsyncExecutor` specialization for thread pools that also handle :class:`contextvars.Context`.
+    """
+
     __slots__ = ()
 
     @overload
@@ -86,9 +168,18 @@ class AsyncThreadPoolExecutor(AsyncExecutor):
         ...
 
     def __init__(self, backend: AsyncBackend, **kwargs: Any) -> None:
+        """
+        Parameters:
+            backend: The asynchronous backend interface.
+            kwargs: see :class:`concurrent.futures.ThreadPoolExecutor` documentation.
+        """
         super().__init__(backend, concurrent.futures.ThreadPoolExecutor(**kwargs))
 
     async def run(self, func: Callable[_P, _T], /, *args: _P.args, **kwargs: _P.kwargs) -> _T:
+        """
+        Similar to :meth:`AsyncExecutor.run`, except that contexts (:class:`contextvars.Context`) are properly propagated
+        to worker threads.
+        """
         ctx = contextvars.copy_context()
 
         if _sniffio_current_async_library_cvar is not None:
