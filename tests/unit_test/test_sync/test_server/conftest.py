9293be4f76878199101de7ea2fcf0d9a75fcdffc
Francis CLAIRICIA-ROSE-CLAIRE-JOSEPHINE <francis.clairicia-rose-claire-josephine@epitech.eu>
2023-12-31 18:05:16 +0100

    [ADD] Added 'runner_options' to standalone servers constructors

---
+++ tests/unit_test/test_sync/test_server/conftest.py
@@ -0,0 +1,37 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from easynetwork.api_async.server.handler import (
+    AsyncDatagramClient,
+    AsyncDatagramRequestHandler,
+    AsyncStreamClient,
+    AsyncStreamRequestHandler,
+)
+
+import pytest
+
+if TYPE_CHECKING:
+    from unittest.mock import MagicMock
+
+    from pytest_mock import MockerFixture
+
+
+@pytest.fixture
+def mock_datagram_request_handler(mocker: MockerFixture) -> MagicMock:
+    return mocker.NonCallableMagicMock(spec=AsyncDatagramRequestHandler)
+
+
+@pytest.fixture
+def mock_stream_request_handler(mocker: MockerFixture) -> MagicMock:
+    return mocker.NonCallableMagicMock(spec=AsyncStreamRequestHandler)
+
+
+@pytest.fixture
+def mock_async_datagram_client(mocker: MockerFixture) -> MagicMock:
+    return mocker.NonCallableMagicMock(spec=AsyncDatagramClient)
+
+
+@pytest.fixture
+def mock_async_stream_client(mocker: MockerFixture) -> MagicMock:
+    return mocker.NonCallableMagicMock(spec=AsyncStreamClient)
