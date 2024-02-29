francis-clairicia/EasyNetwork

commit 6bd749a5573897e460a3991c6867e803bcfaa7c9

Servers: Request handlers can specify the receive timeout by yielding the value (#246)

src/easynetwork/lowlevel/api_async/servers/stream.py
@@ -21,14 +21,14 @@ __all__ = ["AsyncStreamClient", "AsyncStreamServer"]
 import contextlib
 import dataclasses
 from collections.abc import AsyncGenerator, Callable, Mapping
-from typing import Any, Generic, NoReturn, Self
+from typing import Any, Generic, NoReturn
 
 from .... import protocol as protocol_module
 from ...._typevars import _T_Request, _T_Response
 from ....exceptions import UnsupportedOperation
 from ... import _stream, _utils, typed_attr
 from ..._asyncgen import AsyncGenAction, SendAction, ThrowAction
-from ..backend.abc import TaskGroup
+from ..backend.abc import AsyncBackend, TaskGroup
 from ..transports import abc as transports, utils as transports_utils
 
 
@@ -133,7 +133,7 @@ class AsyncStreamServer(typed_attr.TypedAttributeProvider, Generic[_T_Request, _
 
     async def serve(
         self,
-        client_connected_cb: Callable[[AsyncStreamClient[_T_Response]], AsyncGenerator[None, _T_Request]],
+        client_connected_cb: Callable[[AsyncStreamClient[_T_Response]], AsyncGenerator[float | None, _T_Request]],
         task_group: TaskGroup | None = None,
     ) -> NoReturn:
         with self.__serve_guard:
@@ -142,11 +142,13 @@ class AsyncStreamServer(typed_attr.TypedAttributeProvider, Generic[_T_Request, _
 
     async def __client_coroutine(
         self,
-        client_connected_cb: Callable[[AsyncStreamClient[_T_Response]], AsyncGenerator[None, _T_Request]],
+        client_connected_cb: Callable[[AsyncStreamClient[_T_Response]], AsyncGenerator[float | None, _T_Request]],
         transport: transports.AsyncStreamTransport,
     ) -> None:
         if not isinstance(transport, transports.AsyncStreamTransport):
             raise TypeError(f"Expected an AsyncStreamTransport object, got {transport!r}")
+
+        from ..backend.factory import current_async_backend
 
         async with contextlib.AsyncExitStack() as task_exit_stack:
             task_exit_stack.push_async_callback(transports_utils.aclose_forcefully, transport)
@@ -160,12 +162,14 @@ class AsyncStreamServer(typed_attr.TypedAttributeProvider, Generic[_T_Request, _
                     raise UnsupportedOperation  # pragma: no cover
                 consumer = _stream.BufferedStreamDataConsumer(self.__protocol, self.__max_recv_size)
                 request_receiver = _BufferedRequestReceiver(
+                    backend=current_async_backend(),
                     transport=transport,
                     consumer=consumer,
                 )
             except UnsupportedOperation:
                 consumer = _stream.StreamDataConsumer(self.__protocol)
                 request_receiver = _RequestReceiver(
+                    backend=current_async_backend(),
                     transport=transport,
                     consumer=consumer,
                     max_recv_size=self.__max_recv_size,
@@ -178,21 +182,27 @@ class AsyncStreamServer(typed_attr.TypedAttributeProvider, Generic[_T_Request, _
 
             request_handler_generator = client_connected_cb(client)
 
-            del client_exit_stack, task_exit_stack, client_connected_cb, client
+            del client_exit_stack, task_exit_stack, client_connected_cb, client, current_async_backend
 
-            async with contextlib.aclosing(request_handler_generator):
-                try:
-                    await anext(request_handler_generator)
-                except StopAsyncIteration:
-                    return
-
-                async for action in request_receiver:
+            timeout: float | None
+            try:
+                timeout = await anext(request_handler_generator)
+            except StopAsyncIteration:
+                return
+            else:
+                while True:
                     try:
-                        await action.asend(request_handler_generator)
+                        action = await request_receiver.next(timeout)
+                    except StopAsyncIteration:
+                        break
+                    try:
+                        timeout = await action.asend(request_handler_generator)
                     except StopAsyncIteration:
                         break
                     finally:
                         del action
+            finally:
+                await request_handler_generator.aclose()
 
     @property
     def extra_attributes(self) -> Mapping[Any, Callable[[], Any]]:
@@ -201,17 +211,16 @@ class AsyncStreamServer(typed_attr.TypedAttributeProvider, Generic[_T_Request, _
 
 @dataclasses.dataclass(kw_only=True, eq=False, frozen=True, slots=True)
 class _RequestReceiver(Generic[_T_Request]):
+    backend: AsyncBackend
     transport: transports.AsyncStreamReadTransport
     consumer: _stream.StreamDataConsumer[_T_Request]
     max_recv_size: int
+    __null_timeout_ctx: contextlib.nullcontext[None] = dataclasses.field(init=False, default_factory=contextlib.nullcontext)
 
     def __post_init__(self) -> None:
         assert self.max_recv_size > 0, f"{self.max_recv_size=}"  # nosec assert_used
 
-    def __aiter__(self) -> Self:
-        return self
-
-    async def __anext__(self) -> AsyncGenAction[None, _T_Request]:
+    async def next(self, timeout: float | None) -> AsyncGenAction[_T_Request]:
         try:
             consumer = self.consumer
             try:
@@ -221,12 +230,13 @@ class _RequestReceiver(Generic[_T_Request]):
             else:
                 return SendAction(request)
 
-            while data := await self.transport.recv(self.max_recv_size):
-                try:
-                    request = consumer.next(data)
-                except StopIteration:
-                    continue
-                return SendAction(request)
+            with self.__null_timeout_ctx if timeout is None else self.backend.timeout(timeout):
+                while data := await self.transport.recv(self.max_recv_size):
+                    try:
+                        request = consumer.next(data)
+                    except StopIteration:
+                        continue
+                    return SendAction(request)
         except BaseException as exc:
             return ThrowAction(exc)
         raise StopAsyncIteration
@@ -234,13 +244,12 @@ class _RequestReceiver(Generic[_T_Request]):
 
 @dataclasses.dataclass(kw_only=True, eq=False, frozen=True, slots=True)
 class _BufferedRequestReceiver(Generic[_T_Request]):
+    backend: AsyncBackend
     transport: transports.AsyncBufferedStreamReadTransport
     consumer: _stream.BufferedStreamDataConsumer[_T_Request]
+    __null_timeout_ctx: contextlib.nullcontext[None] = dataclasses.field(init=False, default_factory=contextlib.nullcontext)
 
-    def __aiter__(self) -> Self:
-        return self
-
-    async def __anext__(self) -> AsyncGenAction[None, _T_Request]:
+    async def next(self, timeout: float | None) -> AsyncGenAction[_T_Request]:
         try:
             consumer = self.consumer
             try:
@@ -250,12 +259,13 @@ class _BufferedRequestReceiver(Generic[_T_Request]):
             else:
                 return SendAction(request)
 
-            while nbytes := await self.transport.recv_into(consumer.get_write_buffer()):
-                try:
-                    request = consumer.next(nbytes)
-                except StopIteration:
-                    continue
-                return SendAction(request)
+            with self.__null_timeout_ctx if timeout is None else self.backend.timeout(timeout):
+                while nbytes := await self.transport.recv_into(consumer.get_write_buffer()):
+                    try:
+                        request = consumer.next(nbytes)
+                    except StopIteration:
+                        continue
+                    return SendAction(request)
         except BaseException as exc:
             return ThrowAction(exc)
         raise StopAsyncIteration

benchmark_server/servers/easynetwork_udp_echoserver.py
@@ -10,7 +10,7 @@ from typing import Any
 
 from easynetwork.protocol import DatagramProtocol
 from easynetwork.serializers.abc import AbstractPacketSerializer
-from easynetwork.servers.handlers import AsyncDatagramClient, AsyncDatagramRequestHandler
+from easynetwork.servers.handlers import AsyncDatagramClient, AsyncDatagramRequestHandler, INETClientAttribute
 from easynetwork.servers.standalone_udp import StandaloneUDPNetworkServer
 
 
@@ -48,15 +48,13 @@ class EchoRequestHandlerWithTTL(_BaseRequestHandler):
         super().__init__(eager_tasks=eager_tasks)
         self._client_ttl: float = client_ttl
 
-    async def handle(self, client: AsyncDatagramClient[Any]) -> AsyncGenerator[None, Any]:
-        from asyncio import timeout
-
+    async def handle(self, client: AsyncDatagramClient[Any]) -> AsyncGenerator[float | None, Any]:
         client_ttl = self._client_ttl
         while True:
             try:
-                async with timeout(client_ttl):
-                    request = yield
+                request = yield client_ttl
             except TimeoutError:
+                print(f"{client.extra(INETClientAttribute.local_address)}: timed out")
                 return
             await client.send_packet(request)
 

docs/source/_include/examples/howto/tcp_servers/request_handler_explanation.py
@@ -162,7 +162,7 @@ class ClientLoopInRequestHandler(AsyncStreamRequestHandler[Request, Response]):
                 await client.send_packet(Response())
 
 
-class TimeoutRequestHandler(AsyncStreamRequestHandler[Request, Response]):
+class TimeoutContextRequestHandler(AsyncStreamRequestHandler[Request, Response]):
     async def handle(
         self,
         client: AsyncStreamClient[Response],
@@ -171,6 +171,20 @@ class TimeoutRequestHandler(AsyncStreamRequestHandler[Request, Response]):
             async with asyncio.timeout(30):
                 # The client has 30 seconds to send the request to the server.
                 request: Request = yield
+        except TimeoutError:
+            await client.send_packet(TimedOut())
+        else:
+            await client.send_packet(Response())
+
+
+class TimeoutYieldedRequestHandler(AsyncStreamRequestHandler[Request, Response]):
+    async def handle(
+        self,
+        client: AsyncStreamClient[Response],
+    ) -> AsyncGenerator[float | None, Request]:
+        try:
+            # The client has 30 seconds to send the request to the server.
+            request: Request = yield 30.0
         except TimeoutError:
             await client.send_packet(TimedOut())
         else:

docs/source/_include/examples/howto/udp_servers/request_handler_explanation.py
@@ -106,13 +106,13 @@ class MultipleYieldInRequestHandler(AsyncDatagramRequestHandler[Request, Respons
         return True
 
 
-class TimeoutRequestHandler(AsyncDatagramRequestHandler[Request, Response]):
+class TimeoutContextRequestHandler(AsyncDatagramRequestHandler[Request, Response]):
     async def handle(
         self,
         client: AsyncDatagramClient[Response],
     ) -> AsyncGenerator[None, Request]:
-        # It is *never* useful to have a timeout for the 1st datagram because the datagram
-        # is already in the queue.
+        # It is *never* useful to have a timeout for the 1st datagram
+        # because the datagram is already in the queue.
         request: Request = yield
 
         ...
@@ -123,6 +123,28 @@ class TimeoutRequestHandler(AsyncDatagramRequestHandler[Request, Response]):
             async with asyncio.timeout(30):
                 # The client has 30 seconds to send the 2nd request to the server.
                 another_request: Request = yield
+        except TimeoutError:
+            await client.send_packet(TimedOut())
+        else:
+            await client.send_packet(Response())
+
+
+class TimeoutYieldedRequestHandler(AsyncDatagramRequestHandler[Request, Response]):
+    async def handle(
+        self,
+        client: AsyncDatagramClient[Response],
+    ) -> AsyncGenerator[float | None, Request]:
+        # It is *never* useful to have a timeout for the 1st datagram
+        # because the datagram is already in the queue.
+        request: Request = yield None
+
+        ...
+
+        await client.send_packet(Response())
+
+        try:
+            # The client has 30 seconds to send the 2nd request to the server.
+            another_request: Request = yield 30
         except TimeoutError:
             await client.send_packet(TimedOut())
         else:

pyproject.toml
@@ -229,7 +229,7 @@ ignore_missing_imports = true
 
 [tool.pytest.ini_options]
 asyncio_mode = "strict"  # Avoid some unwanted behaviour
-addopts = "--strict-markers -p 'no:anyio'"  # hatch CLI dependencies installs anyio
+addopts = "--strict-markers -p 'no:anyio' -p 'no:benchmark'"  # hatch CLI dependencies installs anyio
 minversion = "7.1.2"
 testpaths = ["tests"]
 norecursedirs = ["scripts"]

src/easynetwork/lowlevel/_asyncgen.py
@@ -27,7 +27,7 @@ _T_Send = TypeVar("_T_Send")
 _T_Yield = TypeVar("_T_Yield")
 
 
-class AsyncGenAction(Generic[_T_Yield, _T_Send], metaclass=ABCMeta):
+class AsyncGenAction(Generic[_T_Send], metaclass=ABCMeta):
     __slots__ = ()
 
     @abstractmethod
@@ -36,7 +36,7 @@ class AsyncGenAction(Generic[_T_Yield, _T_Send], metaclass=ABCMeta):
 
 
 @dataclasses.dataclass(slots=True)
-class SendAction(AsyncGenAction[_T_Yield, _T_Send]):
+class SendAction(AsyncGenAction[_T_Send]):
     value: _T_Send
 
     async def asend(self, generator: AsyncGenerator[_T_Yield, _T_Send]) -> _T_Yield:
@@ -44,7 +44,7 @@ class SendAction(AsyncGenAction[_T_Yield, _T_Send]):
 
 
 @dataclasses.dataclass(slots=True)
-class ThrowAction(AsyncGenAction[_T_Yield, Any]):
+class ThrowAction(AsyncGenAction[Any]):
     exception: BaseException
 
     async def asend(self, generator: AsyncGenerator[_T_Yield, Any]) -> _T_Yield:

src/easynetwork/lowlevel/api_async/servers/datagram.py
@@ -18,6 +18,7 @@ from __future__ import annotations
 
 __all__ = ["AsyncDatagramServer", "DatagramClientContext"]
 
+import contextlib
 import contextvars
 import dataclasses
 import enum
@@ -104,7 +105,9 @@ class AsyncDatagramServer(typed_attr.TypedAttributeProvider, Generic[_T_Request,
 
     async def serve(
         self,
-        datagram_received_cb: Callable[[DatagramClientContext[_T_Response, _T_Address]], AsyncGenerator[None, _T_Request]],
+        datagram_received_cb: Callable[
+            [DatagramClientContext[_T_Response, _T_Address]], AsyncGenerator[float | None, _T_Request]
+        ],
         task_group: TaskGroup | None = None,
     ) -> NoReturn:
         with self.__serve_guard:
@@ -140,7 +143,9 @@ class AsyncDatagramServer(typed_attr.TypedAttributeProvider, Generic[_T_Request,
 
     async def __client_coroutine(
         self,
-        datagram_received_cb: Callable[[DatagramClientContext[_T_Response, _T_Address]], AsyncGenerator[None, _T_Request]],
+        datagram_received_cb: Callable[
+            [DatagramClientContext[_T_Response, _T_Address]], AsyncGenerator[float | None, _T_Request]
+        ],
         datagram: bytes,
         client: _ClientToken[_T_Response, _T_Address],
         task_group: TaskGroup,
@@ -164,39 +169,46 @@ class AsyncDatagramServer(typed_attr.TypedAttributeProvider, Generic[_T_Request,
                 #####################################################################################################
 
                 request_handler_generator = datagram_received_cb(client.ctx)
+                timeout: float | None
 
                 try:
+                    # Ignore sent timeout here, we already have the datagram.
                     await anext(request_handler_generator)
                 except StopAsyncIteration:
                     return
-
-                action: AsyncGenAction[None, _T_Request] = self.__parse_datagram(datagram, self.__protocol)
-                try:
-                    await action.asend(request_handler_generator)
-                except StopAsyncIteration:
-                    return
-                finally:
-                    del action
-
-                del datagram
-                while True:
+                else:
+                    action: AsyncGenAction[_T_Request] = self.__parse_datagram(datagram, self.__protocol)
                     try:
-                        datagram = await client_data.pop_datagram()
-                    except BaseException as exc:
-                        action = ThrowAction(exc)
-                    else:
-                        action = self.__parse_datagram(datagram, self.__protocol)
-                        del datagram
-                    try:
-                        await action.asend(request_handler_generator)
+                        timeout = await action.asend(request_handler_generator)
                     except StopAsyncIteration:
-                        break
+                        return
                     finally:
                         del action
 
+                    del datagram
+                    while True:
+                        try:
+                            with contextlib.nullcontext() if timeout is None else client_data.backend.timeout(timeout):
+                                datagram = await client_data.pop_datagram()
+
+                            action = self.__parse_datagram(datagram, self.__protocol)
+                            del datagram
+                        except BaseException as exc:
+                            action = ThrowAction(exc)
+                        try:
+                            timeout = await action.asend(request_handler_generator)
+                        except StopAsyncIteration:
+                            break
+                        finally:
+                            del action
+                finally:
+                    await request_handler_generator.aclose()
+
     def __on_task_done(
         self,
-        datagram_received_cb: Callable[[DatagramClientContext[_T_Response, _T_Address]], AsyncGenerator[None, _T_Request]],
+        datagram_received_cb: Callable[
+            [DatagramClientContext[_T_Response, _T_Address]], AsyncGenerator[float | None, _T_Request]
+        ],
         client: _ClientToken[_T_Response, _T_Address],
         task_group: TaskGroup,
         default_context: contextvars.Context,
@@ -222,7 +234,7 @@ class AsyncDatagramServer(typed_attr.TypedAttributeProvider, Generic[_T_Request,
     def __parse_datagram(
         datagram: bytes,
         protocol: protocol_module.DatagramProtocol[_T_Response, _T_Request],
-    ) -> AsyncGenAction[None, _T_Request]:
+    ) -> AsyncGenAction[_T_Request]:
         try:
             try:
                 request = protocol.build_packet_from_datagram(datagram)
@@ -267,6 +279,10 @@ class _ClientData:
         self.__state: _ClientState | None = None
         self._queue_condition: ICondition | None = None
         self._datagram_queue: deque[bytes] | None = None
+
+    @property
+    def backend(self) -> AsyncBackend:
+        return self.__backend
 
     @property
     def task_lock(self) -> ILock:

src/easynetwork/servers/async_tcp.py
@@ -372,7 +372,7 @@ class AsyncTCPNetworkServer(AbstractAsyncNetworkServer, Generic[_T_Request, _T_R
     async def __client_coroutine(
         self,
         lowlevel_client: _stream_server.AsyncStreamClient[_T_Response],
-    ) -> AsyncGenerator[None, _T_Request]:
+    ) -> AsyncGenerator[float | None, _T_Request]:
         async with contextlib.AsyncExitStack() as client_exit_stack:
             self.__attach_server()
             client_exit_stack.callback(self.__detach_server)
@@ -401,19 +401,20 @@ class AsyncTCPNetworkServer(AbstractAsyncNetworkServer, Generic[_T_Request, _T_R
             client_exit_stack.callback(logger.log, self.__client_connection_log_level, "%s disconnected", client_address)
             client_exit_stack.push_async_callback(client._force_close)
 
-            request_handler_generator: AsyncGenerator[None, _T_Request]
-            action: AsyncGenAction[None, _T_Request]
+            request_handler_generator: AsyncGenerator[float | None, _T_Request]
+            action: AsyncGenAction[_T_Request]
+            timeout: float | None
 
             _on_connection_hook = self.__request_handler.on_connection(client)
             if isinstance(_on_connection_hook, AsyncGenerator):
                 try:
-                    await anext(_on_connection_hook)
+                    timeout = await anext(_on_connection_hook)
                 except StopAsyncIteration:
                     pass
                 else:
                     while True:
                         try:
-                            action = SendAction((yield))
+                            action = SendAction((yield timeout))
                         except ConnectionError:
                             await _on_connection_hook.aclose()
                             return
@@ -423,7 +424,7 @@ class AsyncTCPNetworkServer(AbstractAsyncNetworkServer, Generic[_T_Request, _T_R
                                 return
                             action = ThrowAction(_utils.remove_traceback_frames_in_place(exc, 1))
                         try:
-                            await action.asend(_on_connection_hook)
+                            timeout = await action.asend(_on_connection_hook)
                         except StopAsyncIteration:
                             break
                         except BaseException as exc:
@@ -432,6 +433,8 @@ class AsyncTCPNetworkServer(AbstractAsyncNetworkServer, Generic[_T_Request, _T_R
                             raise
                         finally:
                             del action
+                finally:
+                    await _on_connection_hook.aclose()
             else:
                 assert inspect.isawaitable(_on_connection_hook)  # nosec assert_used
                 await _on_connection_hook
@@ -453,30 +456,33 @@ class AsyncTCPNetworkServer(AbstractAsyncNetworkServer, Generic[_T_Request, _T_R
             while not client_is_closing():
                 request_handler_generator = new_request_handler(client)
                 try:
-                    await anext(request_handler_generator)
+                    timeout = await anext(request_handler_generator)
                 except StopAsyncIteration:
                     return
-                while True:
-                    try:
-                        action = SendAction((yield))
-                    except ConnectionError:
-                        await request_handler_generator.aclose()
-                        return
-                    except BaseException as exc:
-                        if _utils.is_ssl_eof_error(exc):
+                else:
+                    while True:
+                        try:
+                            action = SendAction((yield timeout))
+                        except ConnectionError:
                             await request_handler_generator.aclose()
                             return
-                        action = ThrowAction(_utils.remove_traceback_frames_in_place(exc, 1))
-                    try:
-                        await action.asend(request_handler_generator)
-                    except StopAsyncIteration:
-                        break
-                    except BaseException as exc:
-                        # Remove action.asend() frames
-                        _utils.remove_traceback_frames_in_place(exc, 2)
-                        raise
-                    finally:
-                        del action
+                        except BaseException as exc:
+                            if _utils.is_ssl_eof_error(exc):
+                                await request_handler_generator.aclose()
+                                return
+                            action = ThrowAction(_utils.remove_traceback_frames_in_place(exc, 1))
+                        try:
+                            timeout = await action.asend(request_handler_generator)
+                        except StopAsyncIteration:
+                            break
+                        except BaseException as exc:
+                            # Remove action.asend() frames
+                            _utils.remove_traceback_frames_in_place(exc, 2)
+                            raise
+                        finally:
+                            del action
+                finally:
+                    await request_handler_generator.aclose()
 
     def __attach_server(self) -> None:
         self.__active_tasks += 1

commit 3b17978d02d8d01fa523858c8e371ee3daca2a52

Stream endpoints: Fixed memory leak regression after dev in PR #246

src/easynetwork/lowlevel/_utils.py
@@ -162,8 +162,8 @@ def check_socket_family(family: int) -> None:
 def check_real_socket_state(socket: ISocket) -> None:
     """Verify socket saved error and raise OSError if there is one
 
-    There is some functions such as socket.send() which do not immediately fail and save the errno
-    in SO_ERROR socket option because the error spawn after the action was sent to the kernel (Something weird)
+    There are some functions such as socket.send() which do not immediately fail and save the errno
+    in SO_ERROR socket option because the error spawns after the action was sent to the kernel (Something weird)
 
     On Windows: The returned value should be the error returned by WSAGetLastError(), but the socket methods always call
     this function to raise an error, so getsockopt(SO_ERROR) will most likely always return zero :)

src/easynetwork/lowlevel/api_async/endpoints/stream.py
@@ -218,6 +218,8 @@ class _DataReceiverImpl(Generic[_T_ReceivedPacket]):
                 return consumer.next(chunk)
             except StopIteration:
                 pass
+            finally:
+                del chunk
 
         raise EOFError("end-of-stream")
 

src/easynetwork/lowlevel/api_async/servers/stream.py
@@ -236,6 +236,8 @@ class _RequestReceiver(Generic[_T_Request]):
                         request = consumer.next(data)
                     except StopIteration:
                         continue
+                    finally:
+                        del data
                     return SendAction(request)
         except BaseException as exc:
             return ThrowAction(exc)

src/easynetwork/lowlevel/api_sync/endpoints/stream.py
@@ -235,14 +235,15 @@ class _DataReceiverImpl(Generic[_T_ReceivedPacket]):
             if not chunk:
                 self._eof_reached = True
                 continue
-            buffer_not_full: bool = len(chunk) < bufsize
             try:
                 return consumer.next(chunk)
             except StopIteration:
                 if timeout > 0:
                     timeout = elapsed.recompute_timeout(timeout)
-                elif buffer_not_full:
+                elif len(chunk) < bufsize:
                     break
+            finally:
+                del chunk
         # Loop break
         if self._eof_reached:
             raise EOFError("end-of-stream")
@@ -280,13 +281,12 @@ class _BufferedReceiverImpl(Generic[_T_ReceivedPacket]):
             if not nbytes:
                 self._eof_reached = True
                 continue
-            buffer_not_full: bool = nbytes < bufsize
             try:
                 return consumer.next(nbytes)
             except StopIteration:
                 if timeout > 0:
                     timeout = elapsed.recompute_timeout(timeout)
-                elif buffer_not_full:
+                elif nbytes < bufsize:
                     break
         # Loop break
         if self._eof_reached: