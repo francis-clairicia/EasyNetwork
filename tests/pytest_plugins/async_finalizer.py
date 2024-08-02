3b3566aee374426b8ca64842a7b2229bbd1a0cbc
Francis CLAIRICIA-ROSE-CLAIRE-JOSEPHINE <francis.clairicia-rose-claire-josephine@epitech.eu>
2024-08-02 14:22:52 +0200

    Testing: Added trio fixture version of async_finalizer

--- tests/pytest_plugins/async_finalizer.py
+++ tests/pytest_plugins/async_finalizer.py
@@ -7,6 +7,8 @@ from typing import Any
 
 import pytest_asyncio
 
+from ..fixtures.trio import trio_fixture
+
 
 @dataclass(repr=False, eq=False, kw_only=True, frozen=True, slots=True, weakref_slot=True)
 class AsyncFinalizer:
@@ -15,6 +17,24 @@ class AsyncFinalizer:
 
 @pytest_asyncio.fixture
 async def async_finalizer(request: Any) -> AsyncIterator[AsyncFinalizer]:
+    import asyncio
+
+    asyncio.get_running_loop()
+
+    async with contextlib.AsyncExitStack() as stack:
+
+        def add_finalizer(f: Callable[[], Awaitable[Any]]) -> None:
+            stack.push_async_callback(f)
+
+        yield AsyncFinalizer(add_finalizer=add_finalizer)
+
+
+@trio_fixture
+async def async_finalizer_trio(request: Any) -> AsyncIterator[AsyncFinalizer]:
+    import trio
+
+    trio.lowlevel.current_trio_token()
+
     async with contextlib.AsyncExitStack() as stack:
 
         def add_finalizer(f: Callable[[], Awaitable[Any]]) -> None:
